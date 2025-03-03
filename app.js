e6b2f595f7555c59be701b2e71f288f23d079665
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-03 14:56:17 +0500

    Adding DB to todolist

--- app.js
+++ app.js
@@ -1,59 +1,73 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const date = require(__dirname +"/date.js");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 
 // setting the ejs as a viewing engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // setting bory parser for post method
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // telling express to serve PUBLIC folder for static resources
 app.use(express.static("public"));
 
-const items = ["Buy Food","Cook Food","Eat Food"];
-const workItems = [];
+const itemSchema = {
+  name: String,
+};
 
-app.get("/", function(req,res){
-    //1. let day = getDate();
-    let day = date.getDay();
-    res.render('list', {listTitle: day, newListItems: items});
+const Item = mongoose.model("item", itemSchema);
 
+const item1 = new Item({
+  name: "Welcome to your ToDo-List",
+});
+const item2 = new Item({
+  name: "Click + to add more items",
+});
+const item3 = new Item({
+  name: "<-- click to delete an item",
 });
 
-app.post("/", function(req, res){
-    let item = req.body.newItem;
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems)
+  .then(() => {
+    console.log("default items added Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", function (req, res) {
+  //1. let day = getDate();
+  let day = date.getDay();
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
     // res.render('list', {newListItem: item});
-        res.redirect("/");
-    }
-    
+    res.redirect("/");
+  }
 });
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle:"Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
-
-
-
-
-
-
-
 
 // const express = require("express");
 // const bodyParser = require("body-parser");
@@ -79,7 +93,7 @@ app.listen(3000, function(){
 //     else{
 //         // res.write("<h1> Boo! Its week day</h1>");
 //         // res.write("<p> I have to work!</p>");
-        
+
 //         // res.sendFile(__dirname + "/index.html");
 //         // res.sendFile(__dirname + "/weekday.html");
 //         res.render('list', {kindOfDay: day});
@@ -90,4 +104,3 @@ app.listen(3000, function(){
 // app.listen(3000, function(){
 //     console.log("Server is running on port 3000");
 // });
-
