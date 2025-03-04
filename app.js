4fe0cc68b3f308a7e1e56129e3518ea4c25ebd88
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-04 16:14:02 +0500

    Adding items in custom lists

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const defaultItems = [item1, item2, item3];
 
 app.get("/", async function (req, res) {
   //1. let day = getDate();
-  let day = date.getDay();
+  //let day = date.getDay();
   let arr = await Item.find();
   if (arr.length == 0) {
     Item.insertMany(defaultItems)
@@ -45,18 +45,28 @@ app.get("/", async function (req, res) {
       });
     res.redirect("/");
   } else {
-    res.render("list", { listTitle: day, newListItems: arr });
+    res.render("list", { listTitle: "Today", newListItems: arr });
   }
 });
 
 app.post("/", function (req, res) {
   let task = req.body.newItem;
+  const listName = req.body.list;
+
   const newtask = new Item({
     name: task,
   });
 
-  newtask.save();
-  res.redirect("/");
+  if (listName === "Today") {
+    newtask.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }).then((foundList) => {
+      foundList.items.push(newtask);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
 app.post("/delete", function (req, res) {
@@ -113,39 +123,3 @@ app.get("/:customListName", async function (req, res) {
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-
-// const app = express();
-
-// // setting the ejs as a viewing engine
-// app.set('view engine', 'ejs');
-
-// app.get("/", function(req,res){
-//     let today = new Date();
-//     let currentDay = today.getDay();
-
-//     // const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-//     // let day = weekday[currentDay];
-
-//     if(currentDay === 6 || currentDay === 0){
-//         // res.write("<h1> Ahha! Its weekend</h1>");
-
-//         // res.sendFile(__dirname + "/weekend.html");
-//         res.render('list', {kindOfDay: day});
-//     }
-//     else{
-//         // res.write("<h1> Boo! Its week day</h1>");
-//         // res.write("<p> I have to work!</p>");
-
-//         // res.sendFile(__dirname + "/index.html");
-//         // res.sendFile(__dirname + "/weekday.html");
-//         res.render('list', {kindOfDay: day});
-//     }
-//     // res.send();
-// });
-
-// app.listen(3000, function(){
-//     console.log("Server is running on port 3000");
-// });
